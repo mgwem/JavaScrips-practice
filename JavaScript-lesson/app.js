ff5767d3d079f1b785e5448069c9f5f4e27585ef
EmiriMaegawa <101238309+mgwem@users.noreply.github.com>
2022-05-03 18:20:14 +0900

    JavaScriptの練習

---
+++ JavaScript-lesson/app.js
@@ -0,0 +1,42 @@
+// let hello = 'Hello World'
+
+// let str1 = 'Hello';
+// let str2 = 'World!!'
+// alert(str1 + str2);
+
+// 条件分岐
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+  // alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+  // alert('みかんとりんごが同じ値段');
+// } else{
+  // alert('みかんの値段がりんごより高い');
+// }
+
+// 繰り返し処理
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+  // num = num * 2
+  // count = count + 1;
+// }
+// alert('2を掛け続けて' + max + 'を越えるのに必要だった回数は' + count + '回です');
+
+// let i;
+// let num = 0;
+
+// for(i = 1;i <11;i++){
+  // num = num + i;
+// }
+
+// alert('1から10まで足し算でした結果は' + num + 'です');
+
+alert(1 + 2);
+alert(3 - 1);
+alert(2 * 2);
+alert(10 / 5)
---
+++ SlideShow-Lesson/js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition:'fade',
+  speed:1000,
+  easing:'easeOutQuart',
+  navType:'block',
+  childrenElementType:'div',
+  arrows:true,
+  autoPlay:true,
+  autoPlayDuraiton:5000,
+  keyboardOnAlways:true,
+  hidePrevious:false
+});
